90bc2842437bd1b7fcef7a6306e39f446f01d8af
Vinayak <vinayak.gaud@gmail.com>
2023-02-16 19:41:55 +0530

    saving image and retreiving data from DB

---
+++ lib/src/main/java/crudoperation/retrieve/FetchingAddressData.java
@@ -0,0 +1,36 @@
+package crudoperation.retrieve;
+
+import org.hibernate.SessionFactory;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.hibernate.Session;
+import sessionprovider.SessionFactoryProvider;
+import tables.UserAddress;
+
+
+public class FetchingAddressData {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		SessionFactory sessionfactory = SessionFactoryProvider.getSessionFactory();
+		Session session = sessionfactory.openSession();
+		
+		UserAddress userAddress = (UserAddress) session.get(UserAddress.class, 1);
+		/**UserAddress userAddress = (UserAddress) session.load(UserAddress.class, 1);
+		we can use loan also but it is deprecated */
+		System.out.println("Fetched data");
+		System.out.println(userAddress.getAddressId());
+		System.out.println(userAddress.getAddressCity());
+		System.out.println(userAddress.getAddressName());
+		System.out.println(userAddress.getAddedDate());
+		System.out.println(userAddress.isOpen());
+		FileOutputStream fos = new FileOutputStream("G:\\JAVA_Projects\\Full Stack Projects\\gradleHibernate\\lib\\src\\main\\java\\images\\fetchedImage\\fetchedImage#"+userAddress.getAddressId()+".png");
+		fos.write(userAddress.getPicture());
+		fos.close();
+		System.out.println("image saved");
+		session.close();
+		sessionfactory.close();
+	}
+
+}
